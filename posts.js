456340eeac99e97d3666858f24af62f0ff24263f
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-06 02:05:25 +0600

    Practice on going

--- posts.js
+++ posts.js
@@ -10,14 +10,23 @@ const loadPosts = () => {
 
 const displayPosts = posts => {
     const postsContainer = document.getElementById('posts_container');
+    postsContainer.innerHTML = '';
 
     posts.forEach((post) => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <h1>${post.id}</h1>
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="flex gap-3 items-center">
+          <div class="">
+            <h1 class="text-6xl">${post.id}</h1>
+          </div>
+          <div class="">
+            <h3 class="text-2xl">${post.title}</h3>
+          <p class="text-lg">${post.body}</p>
+          </div>
+        </div>
         `;
-        postsContainer.appendChild(li);
+        postsContainer.appendChild(div);
     });
 } 
+
+loadPosts();
